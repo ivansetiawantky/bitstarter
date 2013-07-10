feec5df0a76e593c0532a3c193169181d06187d1
ivansetiawantky <ivans@yahoo.co.jp>
2013-07-10 09:39:49 +0000

    grader.js checks URL

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util');
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +67,34 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <path_url>', 'Command line defined URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+    if(program.url != null) {
+        rest.get(program.url).on('complete', function(result) {
+            if(result instanceof Error) {
+                sys.puts('Error: ' + result.message);
+                this.retry(5000);
+            }
+            else {
+                //sys.puts(result);
+                $ = cheerio.load(result);
+                var checks = loadChecks(program.checks);
+                var out = {};
+                for(var ii in checks) {
+                    var present = $(checks[ii]).length > 0;
+                    out[checks[ii]] = present;
+                }
+                var outJson = JSON.stringify(out, null, 4);
+                console.log(outJson);
+            }
+        });
+    }
+    else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
